ba08abea7ead3bd1e381a759179e0dd03959a97d
vignesh <vigneshsuthapalli@gmail.com>
2026-05-10 14:45:58 +0530

    Changes

--- route/index.js
+++ route/index.js
@@ -5,5 +5,9 @@ router.use('/todos', require('./todo'));
 router.use('/otp', require('./otp'));
 router.use('/cronjob', require('./cronjob'));
 router.use('/upload', require('./upload'));
+router.use('/health', (req, res) => {
+ uptime = process.uptime();
+ res.status(200).json({ status: true, message: 'Server is healthy', uptime: `${Math.floor(uptime / 60)} minutes ${Math.floor(uptime % 60)} seconds` });
+});
 
 module.exports = router;
