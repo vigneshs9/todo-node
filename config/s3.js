784cacb8bda60f30373dfa1b72be40ee60086b8c
vignesh <vigneshsuthapalli@gmail.com>
2026-05-10 14:57:41 +0530

    Changes

--- config/s3.js
+++ config/s3.js
@@ -3,8 +3,8 @@ const { S3Client } = require('@aws-sdk/client-s3');
 const s3 = new S3Client({
  region: AWS_REGION,
  credentials: {
-  accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_KEY
+  accessKeyId: S3_ACCESS_KEY,
+  secretAccessKey: S3_SECRET_KEY
  }
 });
 
