86b2ec065638c30c176631ccdf5b6f2d13dc0dbd
vignesh <vigneshsuthapalli@gmail.com>
2026-06-07 15:04:03 +0530

    Refactor validation to use body() instead of check() for consistency in POST and PUT requests

--- route/todo/index.js
+++ route/todo/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const todoController = require('../../controller/todo');
-const { check, validationResult, body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 router
 .post('/', [
- check('title').notEmpty().isString().withMessage('Title is required'),
- check('date').notEmpty().isString().withMessage('Date is required'),
- check('userId').notEmpty().isMongoId().withMessage('userId is required'),
- check('description').notEmpty().isString().withMessage('Description is required'),
- check('taskStatus').notEmpty().isInt().withMessage('taskStatus is required'),
+ body('title').notEmpty().isString().withMessage('Title is required'),
+ body('date').notEmpty().isString().withMessage('Date is required'),
+ body('userId').notEmpty().isMongoId().withMessage('userId is required'),
+ body('description').notEmpty().isString().withMessage('Description is required'),
+ body('taskStatus').notEmpty().isInt().withMessage('taskStatus is required'),
  body('priority').notEmpty().isInt().withMessage('priority is required')
 ], (req, res) => {
  const errors = validationResult(req);
@@ -21,12 +21,12 @@ router
  }
 })
 .put('/', [
- check('todoId').notEmpty().isMongoId().withMessage('todoId is required'),
- check('title').notEmpty().isString().withMessage('Title is required'),
- check('date').notEmpty().isString().withMessage('Date is required'),
- check('description').notEmpty().isString().withMessage('Description is required'),
- check('taskStatus').notEmpty().isInt().withMessage('taskStatus is required'),
- check('priority').notEmpty().isInt().withMessage('priority is required')
+ body('todoId').notEmpty().isMongoId().withMessage('todoId is required'),
+ body('title').notEmpty().isString().withMessage('Title is required'),
+ body('date').notEmpty().isString().withMessage('Date is required'),
+ body('description').notEmpty().isString().withMessage('Description is required'),
+ body('taskStatus').notEmpty().isInt().withMessage('taskStatus is required'),
+ body('priority').notEmpty().isInt().withMessage('priority is required')
 ], (req, res) => {
  const errors = validationResult(req);
  if (!errors.isEmpty()) {
@@ -47,7 +47,7 @@ router.post('/fetch', [
  }
 })
 router.post('/delete', [
- check('todoId').notEmpty().isMongoId().withMessage('todoId is required')
+ body('todoId').notEmpty().isMongoId().withMessage('todoId is required')
 ], (req, res) => {
  const errors = validationResult(req);
  if (!errors.isEmpty()) {
